d1497f64e8e261e4e4ab8b20253ff748ef163d4d
samcid <samuelcidbilbao@gmail.com>
2022-05-30 02:18:20 -0400

    identficadores en inputs, usuarios y tablas en admin de django para probar con sqlLite

--- core/static/core/js/registro.js
+++ core/static/core/js/registro.js
@@ -10,3 +10,41 @@ function activarMenu() {
         $('#titulo').toggleClass('corrido');
     });
 }
+
+function soloNumerosDatos(e) {
+	var key = e.keyCode || e.which,
+		tecla = String.fromCharCode(key).toLowerCase(),
+		letras = "12345678910",
+		especiales = [8, 37, 39, 46],
+		tecla_especial = false;
+
+	for (var i in especiales) {
+		if (key == especiales[i]) {
+			tecla_especial = true;
+			break;
+		}
+	}
+
+	if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+		return false;
+	}
+}
+
+function soloLetras(e) {
+	var key = e.keyCode || e.which,
+		tecla = String.fromCharCode(key).toLowerCase(),
+		letras = " áéíóúabcdefghijklmnñopqrstuvwxyz",
+		especiales = [8, 37, 39, 46],
+		tecla_especial = false;
+
+	for (var i in especiales) {
+		if (key == especiales[i]) {
+			tecla_especial = true;
+			break;
+		}
+	}
+
+	if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+		return false;
+	}
+}
