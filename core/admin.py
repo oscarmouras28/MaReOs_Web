e6a271bb1a818e7da859f54df81cf671b23a7447
deadstar4l <pawerpablo11@gmail.com>
2022-06-06 08:52:48 -0400

    admin enchulado

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from .models import Cliente,Vendedor,Carrito,Venta,Producto,Delivery,Medio_pago
 
+
+
+#Mostrar mas informacion sobre las clases  
+class detallecarrito(admin.ModelAdmin):
+    search_fields = ["producto_id"]
+
+
+class detallecliente(admin.ModelAdmin):
+    search_fields = ["p_nombre"]
+
+
 # Register your models here.
 
-admin.site.register(Cliente)
+admin.site.register(Cliente, detallecliente)
 admin.site.register(Vendedor)
-admin.site.register(Carrito)
+admin.site.register(Carrito, detallecarrito)
 admin.site.register(Venta)
 admin.site.register(Producto)
 admin.site.register(Delivery)
--- core/models.py
+++ core/models.py
@@ -11,6 +11,11 @@ class Cliente (models.Model):
     a_paterno = models.CharField(max_length=20)
     num_contac = models.IntegerField()
     rut = models.CharField(max_length=20)
+    def __str__(self):
+        return self.p_nombre + " " + self.a_paterno 
+
+    class Meta:
+        verbose_name_plural = "Cliente"
 
 
 class Vendedor (models.Model):
@@ -19,10 +24,12 @@ class Vendedor (models.Model):
     a_paterno = models.CharField(max_length=20)
     a_materno = models.CharField(max_length=20)
     rut = models.CharField(max_length=20)
-
+    def __str__(self):
+        return self.p_nombre + " " + self.a_paterno 
     class Meta:
         verbose_name_plural = "Vendedores"
-
+    def __str__(self):
+        return self.p_nombre + " " + self.a_paterno
 
 class Producto (models.Model):
     nombre = models.CharField(max_length=30)
@@ -30,22 +37,31 @@ class Producto (models.Model):
     img = models.ImageField(upload_to="productos")
     desc = models.CharField(max_length=200)
     tip_producto = models.CharField(max_length=1)
-
-
+    class Meta:
+        verbose_name_plural = "Producto"
+    def __str__(self):
+        return self.nombre + " " + self.desc
 class Carrito (models.Model):
     producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE)
-
-
-
+    class Meta:
+        verbose_name_plural = "Carrito"
+    def __str__(self):
+        return self.producto_id.nombre
 class Medio_pago (models.Model):
     tipo_pago = models.CharField(max_length=20)
 
+    class Meta:
+        verbose_name_plural = "Medio de pago"
+    def __str__(self):
+        return self.tipo_pago 
 class Delivery (models.Model):
     direccion = models.CharField(max_length=30)
     telefono = models.IntegerField()
     valor = models.IntegerField()
-    
-
+    class Meta:
+        verbose_name_plural = "Delivery"
+    def __str__(self):
+        return self.direccion 
 class Venta (models.Model):
     #--<date=models.CharField(max_length=30) SE ARREGLA MÑA>--#
     subtotal = models.IntegerField()
@@ -56,4 +72,9 @@ class Venta (models.Model):
     pedido = models.CharField(max_length=30)
     producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE)
     pago_id = models.ForeignKey(Medio_pago, on_delete=models.CASCADE)
-    delivery_id = models.ForeignKey(Delivery, on_delete=models.CASCADE, null=True)
+    delivery_id = models.ForeignKey(
+        Delivery, on_delete=models.CASCADE, null=True)
+    class Meta:
+        verbose_name_plural = "Venta"
+    def __str__(self):
+        return  self.cliente_id + " " + self.pedido
