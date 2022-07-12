3116cf98ab5b11385ea0f6f1ead1ee7b72daaf2a
samcid <samuelcidbilbao@gmail.com>
2022-07-12 02:11:42 -0400

    responsividad y home listo

--- core/static/core/js/home.js
+++ core/static/core/js/home.js
@@ -10,11 +10,7 @@ function activarMenu() {
         $('#sidebar').toggleClass('active');
         $('#carrusel').toggleClass('activo');
         $('#titulo').toggleClass('corrido');
-        $('#tarjetas1').toggleClass('corridas');
-        $('#tarjetas2').toggleClass('corridas');
-        $('#tarjetas3').toggleClass('corridas');
-        $('#tarjetas4').toggleClass('corridas');
-
+        $('#contenidoTarjetas').toggleClass('corridas');
     });
 }
 
